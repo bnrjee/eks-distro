2ab5cf4624c98073d6bfefef3a4756975c0976b7
Abhisek Banerjee <bnrjee@amazon.com>
2021-01-15 00:31:36 +0000

    More changes

--- cmd/main_postsubmit.go
+++ cmd/main_postsubmit.go
@@ -10,6 +10,7 @@ import (
 
 func runCommand(root string, runMake bool, path string, releaseBranch string, release string, artifactBucket string, args []string) {
 	if runMake == true {
+		// Add a specific target
 		args[1] = args[1] + path
 		command := exec.Command("make", args...)
 		output, err := command.CombinedOutput()
@@ -17,18 +18,20 @@ func runCommand(root string, runMake bool, path string, releaseBranch string, re
 			log.Fatalf("There was an error running make: %v. Make output:\n%v", err, string(output))
 		}
 		fmt.Printf("Output of the make command for %v:\n %v", path, string(output))
+		// Remove the target name from the build args
+		args[1] = root + "/projects/"
 		command = exec.Command("bash", root+"/release/lib/create_final_dir.sh", releaseBranch, release, artifactBucket, path)
 		output, err = command.CombinedOutput()
 		if err != nil {
 			log.Fatalf("There was an error running the create_final_dir script: %v. Output:\n%v", err, string(output))
 		}
 		fmt.Printf("Output of the create_final_dir script for %v:\n %v", path, string(output))
-		mvCommand := []string{"-c", "mv", root + "/projects/" + path + "/_output/tar/*", "/logs/artifacts"}
-		command = exec.Command("/bin/bash", mvCommand...)
+		command = exec.Command("/bin/bash", "-c", "mv " + root + "/projects/" + path + "/_output/tar/*" + " /logs/artifacts")
 		output, err = command.CombinedOutput()
 		if err != nil {
 			log.Fatalf("There was an error running mv: %v. Output:\n%v", err, string(output))
 		}
+		fmt.Printf("Successfully moved artifacts to /logs/artifacts directory for %v.\n", path)
 	}
 }
 
