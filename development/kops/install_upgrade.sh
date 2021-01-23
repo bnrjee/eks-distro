507237b95c4312f3b54059b6eac9ca1d4a5c9ea6
Abhisek Banerjee <bnrjee@amazon.com>
2021-01-23 01:33:16 +0000

    Fix installer

--- development/kops/install_upgrade.sh
+++ development/kops/install_upgrade.sh
@@ -1,6 +1,7 @@
 curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops-linux-amd64
 mv kops-linux-amd64 /usr/local/bin/kops
+yum install -y unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install
+./aws/install
