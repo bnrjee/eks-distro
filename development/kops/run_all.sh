99f91e727b5fd278eb88ef050c5211dba2f7739e
Abhisek Banerjee <bnrjee@amazon.com>
2021-01-23 01:28:35 +0000

    Upgrade aws

--- development/kops/run_all.sh
+++ development/kops/run_all.sh
@@ -20,7 +20,7 @@ PRESENT_PATH=`pwd`
 echo "This script will create a cluster, run tests and tear it down"
 cd "$PATH_TO_SCRIPT"
 source ./create_store_name.sh
-./install_kops.sh
+./install_upgrade.sh
 ./create_configuration.sh
 ./create_cluster.sh
 ./set_nodeport_access.sh
