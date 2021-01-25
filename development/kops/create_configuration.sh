dd1f702058bdad16c38f112353ce68457c7b4669
Abhisek Banerjee <bnrjee@amazon.com>
2021-01-25 19:23:20 +0000

    Set env

--- development/kops/create_configuration.sh
+++ development/kops/create_configuration.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eo pipefail
+set -exo pipefail
 
 BASEDIR=$(dirname "$0")
 source ${BASEDIR}/set_k8s_versions.sh
@@ -76,7 +76,13 @@ EOF
 
 echo "Creating ${KOPS_CLUSTER_NAME}.yaml"
 kops toolbox template --template eks-d.tpl --values ./${KOPS_CLUSTER_NAME}/values.yaml > "./${KOPS_CLUSTER_NAME}/${KOPS_CLUSTER_NAME}.yaml"
-aws ec2 describe-availability-zones
+
+test_role_arn=`aws sts get-caller-identity| jq .Arn`
+assume_test_role_output=`aws sts assume-role --role-arn $test_role_arn --role-session-name test-role-session`
+export AWS_ACCESS_KEY_ID=`echo $assume_test_role_output|jq .Credentials.AccessKeyId`
+export AWS_SECRET_ACCESS_KEY=`echo $assume_test_role_output|jq .Credentials.SecretAccessKey`
+export AWS_SESSION_TOKEN=`echo $assume_test_role_output|jq .Credentials.SessionToken`
+
 echo "Creating cluster configuration"
 kops create -f "./${KOPS_CLUSTER_NAME}/${KOPS_CLUSTER_NAME}.yaml"
 
