2f1c4137abd8147aaabcc67f868533c59282fce3
Abhisek Banerjee <bnrjee@amazon.com>
2021-01-25 23:08:35 +0000

    Test

--- development/kops/create_configuration.sh
+++ development/kops/create_configuration.sh
@@ -79,9 +79,9 @@ kops toolbox template --template eks-d.tpl --values ./${KOPS_CLUSTER_NAME}/value
 export AWS_DEFAULT_PROFILE=default
 export AWS_PROFILE=default
 assume_test_role_output=`aws sts assume-role --role-arn $TEST_ROLE_ARN --role-session-name test-role-session`
-export AWS_ACCESS_KEY_ID=`echo $assume_test_role_output|jq .Credentials.AccessKeyId`
-export AWS_SECRET_ACCESS_KEY=`echo $assume_test_role_output|jq .Credentials.SecretAccessKey`
-export AWS_SESSION_TOKEN=`echo $assume_test_role_output|jq .Credentials.SessionToken`
+export AWS_ACCESS_KEY_ID=`echo $assume_test_role_output|jq -r .Credentials.AccessKeyId`
+export AWS_SECRET_ACCESS_KEY=`echo $assume_test_role_output|jq -r .Credentials.SecretAccessKey`
+export AWS_SESSION_TOKEN=`echo $assume_test_role_output|jq -r .Credentials.SessionToken`
 #export AWS_DEFAULT_PROFILE=conformance-test
 #export AWS_PROFILE=conformance-test
 unset AWS_DEFAULT_PROFILE
