4a139145aa766bd7e670e6e47715afd219bf1a5c
Abhisek Banerjee <bnrjee@amazon.com>
2021-01-26 21:26:00 +0000

    Test

--- development/kops/auth.sh
+++ development/kops/auth.sh
@@ -1,13 +1,9 @@
-#export AWS_DEFAULT_PROFILE=default
-#export AWS_PROFILE=default
-#assume_test_role_output=`aws sts assume-role --role-arn $TEST_ROLE_ARN --role-session-name test-role-session --duration-seconds 10000`
-#export AWS_ACCESS_KEY_ID=`echo $assume_test_role_output|jq -r .Credentials.AccessKeyId`
-#export AWS_SECRET_ACCESS_KEY=`echo $assume_test_role_output|jq -r .Credentials.SecretAccessKey`
-#export AWS_SESSION_TOKEN=`echo $assume_test_role_output|jq -r .Credentials.SessionToken`
-#unset AWS_DEFAULT_PROFILE
-#unset AWS_PROFILE
-#unset AWS_SDK_LOAD_CONFIG
-export AWS_SDK_LOAD_CONFIG=1
-env|grep AWS
-cat $AWS_CONFIG_FILE
-aws sts get-caller-identity
+export AWS_DEFAULT_PROFILE=default
+export AWS_PROFILE=default
+assume_test_role_output=`aws sts assume-role --role-arn $TEST_ROLE_ARN --role-session-name test-role-session --duration-seconds 10000`
+export AWS_ACCESS_KEY_ID=`echo $assume_test_role_output|jq -r .Credentials.AccessKeyId`
+export AWS_SECRET_ACCESS_KEY=`echo $assume_test_role_output|jq -r .Credentials.SecretAccessKey`
+export AWS_SESSION_TOKEN=`echo $assume_test_role_output|jq -r .Credentials.SessionToken`
+unset AWS_DEFAULT_PROFILE
+unset AWS_PROFILE
+unset AWS_SDK_LOAD_CONFIG
