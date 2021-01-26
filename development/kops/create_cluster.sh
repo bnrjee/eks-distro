e20376060a320db57be0a7a8e6e64fe4c27a6c81
Abhisek Banerjee <bnrjee@amazon.com>
2021-01-26 00:51:10 +0000

    Test

--- development/kops/create_cluster.sh
+++ development/kops/create_cluster.sh
@@ -13,8 +13,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eo pipefail
-
+set -exo pipefail
+export AWS_DEFAULT_PROFILE=default
+export AWS_PROFILE=default
+assume_test_role_output=`aws sts assume-role --role-arn $TEST_ROLE_ARN --role-session-name test-role-session`
+export AWS_ACCESS_KEY_ID=`echo $assume_test_role_output|jq -r .Credentials.AccessKeyId`
+export AWS_SECRET_ACCESS_KEY=`echo $assume_test_role_output|jq -r .Credentials.SecretAccessKey`
+export AWS_SESSION_TOKEN=`echo $assume_test_role_output|jq -r .Credentials.SessionToken`
+unset AWS_DEFAULT_PROFILE
+unset AWS_PROFILE
+unset AWS_SDK_LOAD_CONFIG
+aws sts get-caller-identity
 BASEDIR=$(dirname "$0")
 source ${BASEDIR}/set_k8s_versions.sh
 
