5328af369300615c1264f9b80988f7c6b69a3320
Abhisek Banerjee <bnrjee@amazon.com>
2021-01-26 21:42:24 +0000

    Test

--- development/kops/install.sh
+++ development/kops/install.sh
@@ -1,4 +1,4 @@
-curl -LO https://github.com/kubernetes/kops/releases/download/1.18.0/kops-linux-amd64
+curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
 chmod +x kops-linux-amd64
 mv kops-linux-amd64 /usr/local/bin/kops
 kops version
