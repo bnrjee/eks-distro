44462c907b2670be5397f58268606f93658b303d
Abhisek Banerjee <bnrjee@amazon.com>
2021-01-26 00:56:14 +0000

    Test

--- development/kops/run_all.sh
+++ development/kops/run_all.sh
@@ -20,6 +20,7 @@ PRESENT_PATH=`pwd`
 echo "This script will create a cluster, run tests and tear it down"
 cd "$PATH_TO_SCRIPT"
 source ./create_store_name.sh
+source ./auth.sh
 ./install.sh
 ./create_configuration.sh
 ./create_cluster.sh
