dcafe7128cbcb1bc9d6c4c81e53f2b9a0ce9d571
Abhisek Banerjee <bnrjee@amazon.com>
2021-02-05 01:47:01 +0000

    Test

--- development/kops/create_cluster.sh
+++ development/kops/create_cluster.sh
@@ -17,21 +17,5 @@ set -exo pipefail
 
 BASEDIR=$(dirname "$0")
 source ${BASEDIR}/set_k8s_versions.sh
-
-kops get ig --name ${KOPS_CLUSTER_NAME} --state ${KOPS_STATE_STORE}| \
-{ \
-  read -r; while read -r line; \
-  do \
-    ig_name=`echo $line|awk '{print $1;}'`; \
-    kops get ig --name ${KOPS_CLUSTER_NAME} $ig_name --state ${KOPS_STATE_STORE} -o yaml > existing_config.yaml; \
-    instanc_profile="arn:aws:iam::051478615782:instance-profile/test-build-devstack-kopsAdminInstanceProfile-OD5ZLBSY5K7B"; \
-    if [[ ${ig_name} == "nodes" ]]; then \
-      instanc_profile="arn:aws:iam::051478615782:instance-profile/test-build-devstack-kopsNodesInstanceProfile-1UCR1AB6B0J3C"; \
-    fi; \
-    sed '/spec:/ a \ \ iam:\n \ \ \ profile: '"${instanc_profile}"'' existing_config.yaml > new_config.yaml; \
-    cat new_config.yaml; \
-    kops replace -f new_config.yaml --state ${KOPS_STATE_STORE} --name ${KOPS_CLUSTER_NAME};\
-  done; \
-}
-kops update cluster --name ${KOPS_CLUSTER_NAME} --state ${KOPS_STATE_STORE} --yes --lifecycle-overrides IAMRole=Ignore,IAMRolePolicy=Ignore,IAMInstanceProfileRole=Ignore
+kops update cluster --name ${KOPS_CLUSTER_NAME} --yes
 echo "Update done."
